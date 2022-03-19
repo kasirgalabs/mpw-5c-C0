c4f0e550fb347306a50025414ac1b6944216a86c
yct000 <yahyacantugrul@gmail.com>
2022-03-19 22:44:55 +0300

    change c0 die size

--- openlane/c0_system/config.tcl
+++ openlane/c0_system/config.tcl
@@ -56,7 +56,7 @@ set ::env(CLOCK_NET) "clk_g"
 set ::env(CLOCK_PERIOD) "20"
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 1600 1600"
+set ::env(DIE_AREA) "0 0 1200 1800"
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
